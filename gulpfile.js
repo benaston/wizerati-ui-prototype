aa889d505cb46f11e250b4d9c64bbedc8be7e25c
Ben Aston <ben@bj.ma>
2015-05-27 22:54:38 +0100

    Addition of working gulpfile.

--- gulpfile.js
+++ gulpfile.js
@@ -1,8 +1,36 @@
+/* globals require, __dirname */
+'use strict';
+
 var gulp = require('gulp');
-var gulpLoadPlugins = require('gulp-load-plugins');
-var plugins = gulpLoadPlugins();
-var lessTask = gulpLoadPlugins();
+var requireDir = require('require-dir');
+var tasks = requireDir('./src/javascripts/gulp-tasks', {
+	recurse: false
+});
 
-gulp.task('less', partial(lessTask, ));
+/**
+ * First the options object containing 
+ * globally useful information is 
+ * intantiated...
+ */
+const options = { basePath: __dirname, distPath: 'dist' };
 
-gulp.task('default');
+/**
+ * ...then the options object is supplied 
+ * to each of the gulp tasks individually
+ * via closure (the tasks are not 
+ * registered with gulp yet)...
+ */
+var configuredTasks = Object.keys(tasks)
+	.map(k => tasks[k](options))
+	.reduce((p, c) => (p[c.name] = c.task, p), {});
+
+/**
+ * ...then the configured tasks are registered 
+ * with gulp, with their dependencies being 
+ * defined too. In this way the definition of 
+ * the tasks remains separate from their 
+ * registration with gulp.
+ */
+gulp.task('less', configuredTasks.less);
+gulp.task('build', ['less']);
+gulp.task('default', ['build']);
--- src/javascripts/gulp-tasks/less.js
+++ src/javascripts/gulp-tasks/less.js
@@ -1,9 +1,28 @@
-module.exports = function(lessPath) {
-    return gulp.src(lessPath)
-        .pipe(less({
-            paths: [
-                path.join(__dirname, 'less', 'includes')
-            ]
-        }))
-        .pipe(gulp.dest('./public/css'));
+/* globals module, require */
+'use strict';
+
+var less = require('gulp-less');
+var rename = require('gulp-rename');
+var path = require('path');
+
+var LESS_PATH = 'src/stylesheets';
+var TARGET_FILE_NAME = 'fuckme.min.css';
+
+module.exports = function(options) {
+
+	var lessPath = path.join(options.basePath, LESS_PATH, '**', '*.less');
+	var distPath = path.join(options.basePath, options.distPath);
+
+	return {
+		name: 'less',
+		task: function() {
+			return this.src(lessPath)
+				.pipe(less({
+					paths: [options.basePath],
+					plugins: [],
+				}))
+				.pipe(rename(TARGET_FILE_NAME))
+				.pipe(this.dest(distPath));
+		},
+	};
 };
